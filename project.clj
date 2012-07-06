a3ec0840e666e8d34b1bf70bbd1a1c39d6d5649c
Frozenlock <frozenlock@gmail.com>
2012-07-05 20:32:09 -0400

    release as 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.frozenlock/gzip64 "1.0.0-SNAPSHOT"
+(defproject org.clojars.frozenlock/gzip64 "1.0.0"
   :description "Gzip and encode in base 64 for easy web transfer"
   :url "https://github.com/Frozenlock/gzip64"
   :dependencies [[org.clojure/clojure "1.3.0"]
